cd68023850106c2247c775ccfdbf6334da9f6dad
alirezaAsadii <alirezaasadi7484@gmail.com>
2023-12-27 19:41:29 +0330

    coding first section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,86 @@
+import "./index.css";
 
-import './index.css';
+import { categoryObj, dataObj } from "./obj.js";
+import { categoryAdd } from "./obj.js";
 
 function App() {
-
+  return (
+    <div className="container">
+      <FirstSection />
+      <SecondSection />
+    </div>
+  );
 }
+
+const FirstSection = () => {
+  return (
+    <div className="first-section">
+      <div className="first-section-container">
+        <Logo img="images/Logo/Logo.png" />
+        <Category title="Category" obj={categoryObj} btnObj={categoryAdd} />
+        <Data title="Data" obj={dataObj} />
+      </div>
+    </div>
+  );
+};
+
+const Logo = ({ img }) => {
+  return (
+    <div>
+      <img src={img} alt="Logo" id="logo" />
+    </div>
+  );
+};
+
+const Category = ({ title, obj, btnObj }) => {
+  return (
+    <div>
+      <h2 className="title">{title}</h2>
+      <ul className="category-ul">
+        {obj.map((item) => (
+          <li className="category-li">
+            <a href="#" className="a-tag">
+              <span>
+                <img src={item.icon} />
+              </span>
+              <span>{item.text}</span>
+            </a>
+          </li>
+        ))}
+        <button className="add-category">
+          <span className="add-item-category-span">
+            <img src={btnObj.icon} />
+          </span>
+          <span className="add-item-category-span">{btnObj.text}</span>
+        </button>
+      </ul>
+    </div>
+  );
+};
+
+const Data = ({ title, obj }) => {
+  return (
+    <div>
+      <h2 className="title">{title}</h2>
+      <ul className="category-ul">
+        {obj.map((item) => (
+          <li className="category-li">
+            <a href="#" className="a-tag">
+              <span>
+                <img src={item.icon} />
+              </span>
+              <span>{item.text}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
+const SecondSection = () => {
+  return <div></div>;
+};
 
 export default App;
---
+++ src/obj.js
@@ -0,0 +1,12 @@
+export let categoryObj = [
+  { id: 1, text: "Home", icon: "./Images/Icon/Home.png" },
+  { id: 2, text: "Family", icon: "./Images/Icon/Family.png" },
+  { id: 3, text: "Job", icon: "./Images/Icon/Job.png" },
+  { id: 4, text: "Sport", icon: "./Images/Icon/Sport.png" },
+];
+export let dataObj = [
+  { id: 1, text: "Statistics", icon: "./Images/Icon/Statistics.png" },
+  { id: 2, text: "Compare", icon: "./Images/Icon/Compare.png" },
+];
+
+export let categoryAdd = { id: 1, text: "Add", icon: "./Images/Icon/Add.png" };
